8bcfdaa8da0672a3a887987bf5db98a0d1f8ee30
tfermoso <tomas.fermoso@gmail.com>
2025-03-19 19:13:54 +0100

    Usuario Model

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(models.Model):
+    # Django crea automáticamente un id autoincremental llamado 'id' 
+    # pero si deseas llamarlo usuario_id, puedes hacerlo explícito:
+    usuario_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)  # "pass" es palabra reservada en Python
+    dni = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
