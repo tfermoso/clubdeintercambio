41b11eff3ed9c4c4fef9647089bc763058cefec5
tfermoso <tomas.fermoso@gmail.com>
2025-03-19 19:48:13 +0100

    formulario de registro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from .forms import RegistroForm
 
 def index(request):
     return HttpResponse("Hello, desde Usuarios.")
 
 def registro(request):
-    return HttpResponse("Hello, desde Registro.")
+    if request.method == "POST":
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            usuario = form.save(commit=False)
+            # Si usas el sistema de autenticación de Django, aquí podrías cifrar la contraseña:
+            # usuario.set_password(form.cleaned_data["password"])
+            usuario.save()
+            # Redirige a la página de login o a otra de éxito
+            return redirect('login')
+    else:
+        form = RegistroForm()
+
+    return render(request, "usuarios/registro.html", {"form": form})
 
 def login(request):
     return HttpResponse("Hello, desde login.")
