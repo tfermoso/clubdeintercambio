39e26e52892a00a98cd0aea0b092593e6f840a08
tfermoso <tomas.fermoso@gmail.com>
2025-03-19 19:24:33 +0100

    Libros Model

--- libros/models.py
+++ libros/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from usuarios.models import Usuario  # Importamos el modelo Usuario
+
+class Libro(models.Model):
+    libro_id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=200)
+    imagen = models.ImageField(upload_to='libros/')
+    descripcion = models.TextField()
+    # Relación: cada libro pertenece a un usuario
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
 
 # Create your models here.
