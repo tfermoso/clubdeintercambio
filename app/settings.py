c577680a32b289b3f328735688c084dd544e6d05
tfermoso <tomas.fermoso@gmail.com>
2025-03-19 19:30:03 +0100

    Prestamo model

--- app/settings.py
+++ app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "libros.apps.LibrosConfig",
     "usuarios.apps.UsuariosConfig",
     'django.contrib.admin',
     'django.contrib.auth',
--- prestamos/models.py
+++ prestamos/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from usuarios.models import Usuario
+from libros.models import Libro
 
-# Create your models here.
+class Estado(models.Model):
+    estado_id = models.AutoField(primary_key=True)
+    descripcion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.descripcion
+
+class Prestamo(models.Model):
+    prestamos_id = models.AutoField(primary_key=True)
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    # Usuario que solicita el préstamo
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    fecha_solicitud = models.DateField(auto_now_add=True)
+    fecha_devolucion = models.DateField(null=True, blank=True)
+    estado = models.ForeignKey(Estado, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Préstamo {self.prestamos_id} - {self.libro.titulo}"
