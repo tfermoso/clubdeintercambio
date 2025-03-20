dcf33377d9d8a7ee2fe2e60feba02e5aee900c0f
tfermoso <tomas.fermoso@gmail.com>
2025-03-20 16:46:09 +0100

    recuperamos libros

--- libros/views.py
+++ libros/views.py
@@ -6,7 +6,7 @@ from .models import Libro
 
 def index(request):
     if(request.session.get('usuario_id')): 
-        libros= Libro.objects.filter("usuario_id=request.session.get('usuario_id')")
+        libros = Libro.objects.filter(usuario_id=request.session.get('usuario_id'))
         return render(request, 'index.html', {'libros': libros}) 
     else:
         #redirijo a la pagina de login
