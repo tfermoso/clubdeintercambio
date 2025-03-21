da9d13675dfef19e2f54cb52b23b7ec65cb3a1c5
tfermoso <tomas.fermoso@gmail.com>
2025-03-21 18:01:37 +0100

    libros para prestamo

--- prestamos/views.py
+++ prestamos/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-
+from .models import Libro
 
 def index(request):
-    return render(request, 'index.html')
+    if(request.session.get('usuario_id')): 
+        #obtengo los libros que no son del usuario logeado
+        libros_disponibles = Libro.objects.exclude(usuario_id=request.session.get('usuario_id')) 
+        
+        return render(request, 'prestamos/index.html', {'libros': libros_disponibles}) 
+    else:
+        #redirijo a la pagina de login
+        return redirect('usuarios:login')
+    
