ecd6690d8b6be0a3d90b32491a4d23c496032069
tfermoso <tomas.fermoso@gmail.com>
2025-03-21 20:03:08 +0100

    mostramos solicitudes

--- prestamos/views.py
+++ prestamos/views.py
@@ -6,8 +6,9 @@ def index(request):
     if(request.session.get('usuario_id')): 
         #obtengo los libros que no son del usuario logeado y excluir los libros prestados (estado=3)
         libros_disponibles = Libro.objects.exclude(usuario_id=request.session.get('usuario_id')).exclude(libro_id__in=Prestamo.objects.filter(estado_id=3).values('libro_id'))           
-        
-        return render(request, 'prestamos/index.html', {'libros': libros_disponibles}) 
+        #obtengo los libros que he solicitado
+        solicitudes_prestamos = Prestamo.objects.filter(usuario_id=request.session.get('usuario_id'))
+        return render(request, 'prestamos/index.html', {'libros': libros_disponibles, 'solicitudes_prestamos': solicitudes_prestamos}) 
     else:
         #redirijo a la pagina de login
         return redirect('usuarios:login')
